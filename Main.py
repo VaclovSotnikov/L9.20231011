b1d4b34949c616963d204e25b0497f53ae191ed6
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-10-11 19:35:47 +0300

    new lesson

---
+++ Main.py
@@ -0,0 +1,13 @@
+# while True:
+#     user_input = input("Enter your name: ")
+#     if len(user_input) != 0:
+#         break
+# print(f"You entered {user_input }")
+
+# Create a program witch takes 5 different (minmum)  variables containing strings for username and password.
+# The string should look like : "username='', password=''; username='',password='';....etc"
+# Start Endless loop allowing to enter username and password and 
+# check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
+# The program should show numbers of times you tried to enter both credentials.
+
+credentials = ("username='Juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='Matrix999', password= 'neoliks'; username='")
