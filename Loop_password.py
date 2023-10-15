eeb31e476c6c6e1d35ebda32064daac8df85388e
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-10-15 14:06:14 +0300

    added new lines

---
+++ Loop_password.py
@@ -0,0 +1,22 @@
+
+# Create a program witch takes 5 different (minmum)  variables containing strings for username and password.
+# The string should look like : "username='', password=''; username='',password='';....etc"
+# Start Endless loop allowing to enter username and password and 
+# check if credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
+# The program should show numbers of times you tried to enter both credentials.
+
+credentials = input("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
+username= input("Enter your username: ")
+password= input("Enter your password: ") 
+
+split_list = credentials.split("; ")
+my_dict ={}
+my_dict = split_list
+
+while True:
+    if username in my_dict:
+        if password in my_dict:
+            print(f'Greetings, {username}')
+        break    
+    else:
+        print(f'Try another username or password')
