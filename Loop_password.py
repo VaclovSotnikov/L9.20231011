64a073f54100fe333679fe4803d7824a24c3dda2
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-10-15 17:13:39 +0300

    New line

--- Loop_password.py
+++ Loop_password.py
@@ -5,18 +5,30 @@
 # check if credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
 # The program should show numbers of times you tried to enter both credentials.
 
-credentials = input("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
-username= input("Enter your username: ")
-password= input("Enter your password: ") 
+credentials = ("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
+# username= input("Enter your username: ")
+# password= input("Enter your password: ") 
 
-split_list = credentials.split("; ")
-my_dict ={}
-my_dict = split_list
+# split_list = credentials.split("; ")
+# my_dict ={}
+# my_dict = split_list
 
+# while True:
+#     if username in my_dict:
+#         if password in my_dict:
+#             print(f'Greetings, {username}')
+#         break    
+#     else:
+#         print(f'Try another username or password')
+
+print('Enter correct username and password combo to continue')
+count=1
 while True:
-    if username in my_dict:
-        if password in my_dict:
-            print(f'Greetings, {username}')
-        break    
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    if password=='Hytu76E' and username=='bank_admin':
+        print('Access granted')
+        break
     else:
-        print(f'Try another username or password')
+        print('Access denied. Try again.')
+        count += 1
