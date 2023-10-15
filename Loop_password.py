e7181e1f6a80833cba8dec9ee8028672f8250975
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-10-15 17:39:12 +0300

    new atempt

--- Loop_password.py
+++ Loop_password.py
@@ -5,7 +5,7 @@
 # check if credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
 # The program should show numbers of times you tried to enter both credentials.
 
-credentials = ("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
+# credentials = ("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
 # username= input("Enter your username: ")
 # password= input("Enter your password: ") 
 
@@ -21,6 +21,8 @@ credentials = ("username='juozas007', password='hello11111'; username='gerlix', 
 #     else:
 #         print(f'Try another username or password')
 
+
+credentials = ("username='juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='matrix999', password='neoliks'; username='traukinys', password='garvezis'; username='BX500', password='History'")
 print('Enter correct username and password combo to continue')
 count=1
 while True:
