c83f0d20361310f06a60416944e9516eb6caca03
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-10-19 23:40:04 +0300

    Update

--- Main.py
+++ Main.py
@@ -7,7 +7,42 @@
 # Create a program witch takes 5 different (minmum)  variables containing strings for username and password.
 # The string should look like : "username='', password=''; username='',password='';....etc"
 # Start Endless loop allowing to enter username and password and 
-# check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
+# check if credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials' and ask to enter password and username again. 
 # The program should show numbers of times you tried to enter both credentials.
 
-credentials = ("username='Juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='Matrix999', password= 'neoliks'; username='")
+# credentials = ("username='Juozas007', password='hello11111'; username='gerlix', password='gggg!4'; username='Matrix999', password= 'neoliks'; username='")
+
+# names = ["Albert", "Tom", "Leonardo"]
+# for name in names:
+#     print(f"Greetings, {name}")
+
+# name = "Code Academy"
+# for character in name:
+#     print(character)
+
+# numbers = [1, 2, 3]
+
+# for number in numbers:
+#     print(number)
+
+# print("====")
+
+# for number in range(10, 0, -2):
+#     print(number)    
+
+# for _ in range(10): 
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+# print("what the hell has happened here?!")
+
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)  
